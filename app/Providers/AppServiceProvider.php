73af144f3ac515851c67ff9c36554c2a75c92903
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-04-23 12:04:13 +0100

    comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -54,7 +54,7 @@ class AppServiceProvider extends ServiceProvider
             View::share('membershipCriteria', MembershipCriteria::first());
         }
 
-        View::share('menuItems', MenuItem::with('dropdownItems.allChildren')->get());
+        // View::share('menuItems', MenuItem::with('dropdownItems.allChildren')->get());
 
         $randomMenuItems = MenuItem::with('dropdownItems')->get()->random(5);
         View::share('randomMenuItems', $randomMenuItems);
