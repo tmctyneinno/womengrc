3d8d12bb7ba2a7d0f94d77adcc46cff062251f48
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-05-02 14:30:39 +0100

    comment

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     { 
-        $users = User::latest()->paginate(20);
+        $users = User::latest()->paginate(50);
         return view('admin.users.index', compact('users'));
     }
 
